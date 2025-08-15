9429089b5d5fb31c5a5feab3da311f5d74c5510d
jhasbdmf <oshtyria@uni-osnabrueck.de>
2025-08-15 22:05:36 +0200

    refactor: training method has 1 redundant param less

--- numpy_mlp.py
+++ numpy_mlp.py
@@ -7,25 +7,25 @@ def z_normalize_images(images):
     return (images - mean) / (std + eps)
 
 class MLP ():
-    def __init__(self, input_dim=2304, hidden_dim=16, num_classes=7):
+    def __init__(self, input_dim=2304, hidden_dim=32, num_classes=7):
         rng = np.random.default_rng(seed=42) 
         self.layer1 = rng.normal(
-            loc=0.0,          # mean
-            scale=0.2,        # standard deviation
+            #mean
+            loc=0.0,      
+            #standard deviation
+            scale=0.2,        
             size=(input_dim, hidden_dim)
         ).astype(np.float32)
         self.layer2 = rng.normal(
-            loc=0.0,          # mean
-            scale=0.2,        # standard deviation
+            loc=0.0,          
+            scale=0.2,       
             size=(hidden_dim, num_classes)
         ).astype(np.float32)
 
     def ReLU (self, x):
         x = np.asarray(x)
         return np.maximum(0, x)
-        #if x >= 0: return 1
-        #else: return 0
-
+ 
     def _get_normalized_logits_with_softmax_denom(self, logits):
         softmax_denominator = np.sum(np.exp(logits))
         return np.exp(logits) / softmax_denominator, softmax_denominator
@@ -50,9 +50,8 @@ class MLP ():
             return CEL_value, dW2, dW1
 
 
-def train_model_with (model, 
+def train_model_with_SGD (model, 
                          training_set,
-                         training_set_length,
                          lr: float, 
                          n_epochs: int, 
                          sgd_lr_multiplier: float = 0.95
@@ -81,7 +80,7 @@ def train_model_with (model,
         
         
 
-        total_loss /= training_set_length
+        total_loss /= len(training_set)
         print (f"average loss is {total_loss}")
         loss_history.append(total_loss)
         print ("_" * 50)
@@ -103,20 +102,20 @@ train_y = train_y[perm]
 
 
 
-# z_normalize using the training set stats!
+# z_normalization of inputs
 train_x  = z_normalize_images(train_x)  
-test_x  = z_normalize_images(test_x)  # (optionally: use train mean/std on test_x for true ML practice)
+test_x  = z_normalize_images(test_x)  
 
 #create training and test set by zipping 
 #corresponding inputs and targets
 training_set = zip(train_x, train_y)
 test_set = zip(test_x, test_y)
 
-#numpy printing instruction
+#numpy printing instruction for decimal notation
 np.set_printoptions(
-    precision   = 18,       # how many digits after the decimal
-    floatmode   = 'fixed',  # use fixed (vs scientific or mixed)
-    suppress    = True     # don’t suppress small numbers to 0.000000
+    precision   = 18,       
+    floatmode   = 'fixed',  
+    suppress    = True     
 )
 
 
@@ -128,9 +127,8 @@ SGD_LEARNING_RATE = 2e-3
 LEARNING_RATE_MULTIPLIER_PER_EPOCH = 0.99
 print (f"LR multipliter per epoch = {LEARNING_RATE_MULTIPLIER_PER_EPOCH}")
 N_EPOCHS = 10
-mlp, loss_history_SGD = train_model_with (mlp,
+mlp, loss_history_SGD = train_model_with_SGD (mlp,
                                             list(training_set),
-                                            len(train_x),
                                             SGD_LEARNING_RATE,
                                             N_EPOCHS,
                                             LEARNING_RATE_MULTIPLIER_PER_EPOCH
