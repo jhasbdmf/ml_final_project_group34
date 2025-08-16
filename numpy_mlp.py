56b766a60a5fd0bfffca797ce4e9fede10df9ac5
jhasbdmf <oshtyria@uni-osnabrueck.de>
2025-08-16 09:15:01 +0200

    refactor: old code removed

--- numpy_mlp.py
+++ numpy_mlp.py
@@ -9,7 +9,7 @@ def z_normalize_images(images):
 class MLP ():
     #input layer is not counted in n_layers
     #output layer is
-    def __init__(self, input_dim=2304, n_layers=4, hidden_dim=32, n_classes=7):
+    def __init__(self, input_dim=2304, n_layers=3, hidden_dim=32, n_classes=7):
         rng = np.random.default_rng(seed=42) 
         
         self.n_layers = n_layers
@@ -30,20 +30,6 @@ class MLP ():
                 size=(in_dim, out_dim)
             ).astype(np.float32)
             self.layers.append (current_layer)
-        """
-        self.layer1 = rng.normal(
-            #mean
-            loc=0.0,      
-            #standard deviation
-            scale=0.2,        
-            size=(input_dim, hidden_dim)
-        ).astype(np.float32)
-        self.layer2 = rng.normal(
-            loc=0.0,          
-            scale=0.2,       
-            size=(hidden_dim, n_classes)
-        ).astype(np.float32)
-        """
 
     def ReLU (self, x):
         x = np.asarray(x)
@@ -70,10 +56,7 @@ class MLP ():
             #of the last layer later if necessary
             else:
                 logits = hidden_layer_activations[i-1] @ self.layers[i]
-        """
-        hidden_activations = self.ReLU(inputs @ self.layer1)
-        logits = hidden_activations @ self.layer2
-        """
+
         #if no target value is passed to the model.forward
         #then the model is in the inference mode
         #no logit/output normalization/softmax is necessary
@@ -117,7 +100,11 @@ class MLP ():
             for i in range(self.n_layers-1, -1, -1):
                 #output softmax layer
                 if i == (self.n_layers-1):
+                    #this is the part of the gradient
+                    #which is reused across layers
                     dynamic_gradient = d_softmax
+
+                    #this one contrains gradient of i-th layer
                     layer_gradients[i] = np.outer(hidden_layer_activations[i-1], dynamic_gradient)
                 else:
                     dynamic_gradient = self.layers[i+1] @ dynamic_gradient
@@ -129,19 +116,7 @@ class MLP ():
                     #neither output nor input layer
                     else:
                         layer_gradients[i] = np.outer(hidden_layer_activations[i-1], dynamic_gradient)
-                  
-
-
             return CEL_value, layer_gradients
-            #d_W2 = np.outer(hidden_layer_activations[0], d_softmax)
-            #relu_grad = (hidden_layer_activations[0]>0).astype(float)
-            #d_W1 = np.outer(inputs, (self.layers[1] @ d_softmax)*relu_grad)
-            #dW2 = np.outer(hidden_activations, d_softmax)
-            #relu_grad = (hidden_activations > 0).astype(float)
-            #dW1 = np.outer(inputs, (self.layer2 @ d_softmax)*relu_grad)
-            
-            #return CEL_value, d_W2, d_W1
-
 
 def train_model_with_SGD (model, 
                          training_set,
@@ -162,35 +137,24 @@ def train_model_with_SGD (model,
         for x,y in training_set:
 
             #get the CEL gradient from the forward pass directly
-            #loss, dW2, dW1 = model.forward(x, y)
             loss, layer_grads = model.forward(x, y)
          
             #do SGD step
             for i in range(model.n_layers):
                 model.layers[i] -= lr*layer_grads[i]
 
-            #model.layers[0] -= lr*layer_grads[0]
-            #model.layers[1] -= lr*layer_grads[1]
-            #model.layers[0] -= lr*dW1
-            #model.layers[1] -= lr*dW2
-            #model.layer1 -= lr*dW1
-            #model.layer2 -= lr*dW2
-
             total_loss += loss
           
         #decrease lr each epoch
         lr *= sgd_lr_multiplier
         
-        
-
-        total_loss /= len(training_set)
-        print (f"average loss is {total_loss}")
-        loss_history.append(total_loss)
+        #compute, print and save avg loss per epoch
+        avg_loss = total_loss / len(training_set)
+        print (f"average loss is {avg_loss}")
+        loss_history.append(avg_loss)
         print ("_" * 50)
 
     return model, loss_history
-
-
 
 
 train_x = np.load('train_mlp_x.npy') 
@@ -222,9 +186,7 @@ np.set_printoptions(
 )
 
 
-
 mlp = MLP()
-
 
 SGD_LEARNING_RATE = 2e-3
 LEARNING_RATE_MULTIPLIER_PER_EPOCH = 0.99
